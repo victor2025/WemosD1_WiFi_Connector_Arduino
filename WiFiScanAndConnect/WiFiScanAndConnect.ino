050dd648e7bf501c377e8347d5e5b71eef32ecb2
victor2025 <52420466+victor2025@users.noreply.github.com>
2021-01-21 23:08:51 +0800

    0.4.0 加入网络扫描功能

--- WiFiScanAndConnect/WiFiScanAndConnect.ino
+++ WiFiScanAndConnect/WiFiScanAndConnect.ino
@@ -108,6 +108,31 @@ void wifiStatus(){
     Serial.println("#**************************#\n");
 }
 
+void wifiScan(){
+    Serial.println("#**************************#");
+    Serial.println("Scan start");
+    int n = WiFi.scanNetworks();
+    Serial.println("Scan done");
+    if (n == 0) {
+        Serial.println("No networks found");
+    } else {
+        Serial.print(n);
+        Serial.println(" Networks found:");
+        for (int i = 0; i < n; ++i) {
+            // Print SSID and RSSI for each network found
+            Serial.print(i + 1);
+            Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+            delay(10);
+        }
+    }
+    Serial.println("#**************************#\n");
+}
+
 void loop(){
     serialEvent();
     //指示打印
@@ -185,6 +210,12 @@ void loop(){
                 }
                 else if(cmd == "status"){
                     wifiStatus();
+                    cmd = "";
+                    guide = 'c';
+                    cmdPool = false;
+                }
+                else if(cmd == "scan"){
+                    wifiScan();
                     cmd = "";
                     guide = 'c';
                     cmdPool = false;
